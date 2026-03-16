9c4f76864b7fbba497687eb4c0e9f3c3af0fe07b
Роман <bossssmann@gmail.com>
2026-03-16 11:01:32 +1000

    Create Features.tsx with premium bento grid features

---
+++ src/components/sections/Features.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Features = () => {
+    return (
+        <div className='bento-grid'>
+            <div className='feature-item'>
+                <h3>Feature One</h3>
+                <p>Description of Feature One.</p>
+            </div>
+            <div className='feature-item'>
+                <h3>Feature Two</h3>
+                <p>Description of Feature Two.</p>
+            </div>
+            <div className='feature-item'>
+                <h3>Feature Three</h3>
+                <p>Description of Feature Three.</p>
+            </div>
+            <div className='feature-item'>
+                <h3>Feature Four</h3>
+                <p>Description of Feature Four.</p>
+            </div>
+        </div>
+    );
+};
+
+export default Features;
