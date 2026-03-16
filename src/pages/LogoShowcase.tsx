ae60bc6425b22d13fd7ec121164eeca7065727c6
Роман <bossssmann@gmail.com>
2026-03-16 15:04:48 +1000

    Create LogoShowcase component

---
+++ src/pages/LogoShowcase.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const LogoShowcase: React.FC = () => {
+    return (
+        <div>
+            <h1>Logo Showcase</h1>
+            <p>This is a showcase of logos.</p>
+        </div>
+    );
+};
+
+export default LogoShowcase;
