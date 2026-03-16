6a56517a3d99c6c5a2ca40b2f5daa8a36a38c94a
Роман <bossssmann@gmail.com>
2026-03-16 11:01:12 +1000

    Create premiumCars.ts with premium cars data

---
+++ src/data/premiumCars.ts
@@ -0,0 +1,9 @@
+const premiumCars = [
+    { make: 'Ferrari', model: '488 Spider', year: 2020, price: 250000 },
+    { make: 'Lamborghini', model: 'Huracan', year: 2021, price: 250000 },
+    { make: 'Porsche', model: '911 Turbo', year: 2020, price: 180000 },
+    { make: 'Maserati', model: 'Quattroporte', year: 2021, price: 130000 },
+    { make: 'Aston Martin', model: 'DB11', year: 2020, price: 205000 }
+];
+
+export default premiumCars;
